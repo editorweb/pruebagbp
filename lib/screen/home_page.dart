ac5e4a85766df3e8b5781a2cb1f2b502c012d341
unknown <jorge.r@msn.com>
2020-07-08 19:01:23 -0500

    agrego drawer y logo a drawer

--- lib/screen/home_page.dart
+++ lib/screen/home_page.dart
@@ -86,7 +86,36 @@ class _HomeScreenState extends State<HomeScreen> {
            )
          ],
       ),
-    
+     drawer: Drawer( 
+       child: ListView(
+          padding: EdgeInsets.zero,
+    children: <Widget>[
+      DrawerHeader(
+        child: Text('Drawer Header'),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+                image: ExactAssetImage('assets/images/logo.png'),
+                fit: BoxFit.cover,
+              ),
+        ),
+      ),
+      ListTile(
+        title: Text('Item 1'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+      ListTile(
+        title: Text('Item 2'),
+        onTap: () {
+          // Update the state of the app.
+          // ...
+        },
+      ),
+    ],
+       ),
+    ),
       body: FutureBuilder<List<Photo>>(
         future: fetchPhotos(http.Client()),
         builder: (context, snapshot) {
